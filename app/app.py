efaed20bc2aa8b56f35ded66702ec22da9b2118e
JOEL_ANTONY_JOSE <joelantonyjose08@gmail.com>
2026-06-11 10:33:32 +0530

    Day 3 completed - multiple pages and navigation

--- app/app.py
+++ app/app.py
@@ -6,5 +6,17 @@ app= Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/report")
+def report():
+    return render_template("report.html")
+
+@app.route("/issues")
+def issues():
+    return render_template("issues.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__=="__main__":
     app.run(debug=True)
